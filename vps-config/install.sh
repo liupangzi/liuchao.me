94a05819ff9d26cbd36a14620f4dc71a35717281
holyghost <thesedays@126.com>
2016-04-22 09:46:36 +0800

    update install script

--- vps-config/install.sh
+++ vps-config/install.sh
@@ -4,11 +4,13 @@ OPENRESTY_VERSION="1.9.7.4"
 PHP_VERSION="7.0.5"
 PERCONA_URL="https://www.percona.com/downloads/Percona-Server-5.7/Percona-Server-5.7.11-4/binary/tarball/Percona-Server-5.7.11-4-Linux.x86_64.ssl101.tar.gz"
 PERCONA="Percona-Server-5.7.11-4-Linux.x86_64.ssl101"
+ES_VERSION="2.3.1"
 
 yum install -y epel-release
 yum update -y
 yum install -y \
     wget \
+    unzip \
     git \
     ntpdate \
     gcc gcc-c++ \
@@ -36,6 +38,10 @@ yum install -y \
 mkdir -p /var/www/ /var/log/{nginx,php,mysql,supervisor}
 git clone https://github.com/liupangzi/liuchao.me.git /var/www/liuchao.me
 chown -R nobody:nobody /var/www/liuchao.me
+
+# os
+rm /etc/sysctl.conf && ln -s /var/www/liuchao.me/vps-config/os/sysctl.conf /etc/sysctl.conf
+rm /root/.bashrc && ln -s /var/www/liuchao.me/vps-config/os/dotbashrc /root/.bashrc && source /root/.bashrc
 
 # letsencrypt
 git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
@@ -68,6 +74,7 @@ rm -f /opt/openresty/nginx/conf/nginx.conf
 ln -s /var/www/liuchao.me/vps-config/openresty/conf/nginx.conf /opt/openresty/nginx/conf/nginx.conf
 ln -s /var/www/liuchao.me/vps-config/openresty/conf/servers /opt/openresty/nginx/conf/servers
 ln -s /var/www/liuchao.me/vps-config/openresty/conf/common /opt/openresty/nginx/conf/common
+chown -R nobody:nobody /opt/openresty*
 
 # install PHP
 wget http://hk1.php.net/get/php-${PHP_VERSION}.tar.gz/from/this/mirror -O /data/php-${PHP_VERSION}.tar.gz
@@ -104,6 +111,7 @@ ln -s /opt/php-${PHP_VERSION} /opt/php
 ln -s /var/www/liuchao.me/vps-config/php/php-fpm.conf /opt/php/etc/php-fpm.conf
 ln -s /var/www/liuchao.me/vps-config/php/php.ini /opt/php/lib/php.ini
 ln -s /var/www/liuchao.me/vps-config/php/cert.pem /opt/php/etc/cert.pem
+chown -R nobody:nobody /opt/php*
 
 # install percona
 groupadd mysql && useradd mysql -g mysql
@@ -112,7 +120,41 @@ tar zxf /data/${PERCONA}.tar.gz -C /opt/
 ln -s /opt/${PERCONA} /opt/percona-server
 rm -f /etc/my.cnf && ln -s /var/www/liuchao.me/vps-config/mysql/my.cnf /etc/my.cnf
 /opt/percona-server/bin/mysqld --initialize-insecure
-chown -R mysql:mysql /var/log/mysql/
+chown -R mysql:mysql /var/log/mysql/ /opt/percona-server /opt/Percona-Server*
+
+# install JDK
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u74-b02/jdk-8u74-linux-x64.tar.gz -O /data/jdk-8u74-linux-x64.tar.gz
+tar zxf /data/jdk-8u74-linux-x64.tar.gz -C /opt/
+ln -s /opt/jdk1.8.0_74 /opt/jdk
+chown -R nobody:nobody /opt/jdk*
+
+# install elasticsearch
+groupadd es && useradd es -g es
+wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/${ES_VERSION}/elasticsearch-${ES_VERSION}.tar.gz -O /data/elasticsearch-${ES_VERSION}.tar.gz
+tar zxf /data/elasticsearch-${ES_VERSION}.tar.gz -C /opt/
+ln -s /opt/elasticsearch-${ES_VERSION} /opt/elasticsearch
+
+# install elsaticsearch ik plugin
+mkdir -p /opt/elasticsearch/plugins/ik
+wget https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v1.9.1/elasticsearch-analysis-ik-1.9.1.zip -O /opt/elasticsearch/plugins/ik
+cd /opt/elasticsearch/plugins/ik && unzip elasticsearch-analysis-ik-1.9.1.zip
+
+# update elasticsearch synonym config
+mkdir -p /opt/elasticsearch/config/analysis
+ln -s /var/www/liuchao.me/vps-config/elasticsearch/synonym.txt /opt/elasticsearch/config/analysis/synonym.txt
+
+# install plugin marvel
+su es /opt/elasticsearch/bin/plugin install license
+su es /opt/elasticsearch/bin/plugin install marvel-agent
+
+chown -R es:es /opt/elasticsearch*
+
+# install kibana
+wget https://download.elastic.co/kibana/kibana/kibana-4.5.0-linux-x64.tar.gz -O /data/kibana-4.5.0-linux-x64.tar.gz
+tar zxf /data/kibana-4.5.0-linux-x64.tar.gz -C /opt/
+ln -s /opt/kibana-4.5.0-linux-x64 /opt/kibana
+/opt/kibana/bin/kibana plugin --install elasticsearch/marvel/latest
+chown -R es:es /opt/kibana*
 
 # install supervisor
 yum install -y python-pip python-meld3
@@ -121,7 +163,6 @@ ln -s /var/www/liuchao.me/vps-config/supervisor/supervisord.conf /etc/supervisor
 
 # RUN!
 chown -R nobody:nobody /var/www/liuchao.me
-chown -R nobody:nobody /opt
 /usr/bin/supervisord -c /etc/supervisord.conf
 
 # init MySQL data

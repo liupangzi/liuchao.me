6de56bd02290f8e0861101edc96d15aa93efc3c5
holyghost <thesedays@126.com>
2016-12-12 21:24:47 +0800

    backup

--- tech/wp-content/themes/express/assets/js/_main.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-  "use strict";
-	$('ul.nav li.dropdown').hover(function() {
-		$(this).find('.dropdown-menu').stop(true, true).delay(30).fadeIn();
-	}, function() {
-		$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
-	});
-})(jQuery);

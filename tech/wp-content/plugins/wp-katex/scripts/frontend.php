d4791a3c45066c09e4383a6bd6425ae4540e790f
holyghost <thesedays@126.com>
2017-01-29 11:14:49 +0000

    backup

--- tech/wp-content/plugins/wp-katex/scripts/frontend.php
+++ tech/wp-content/plugins/wp-katex/scripts/frontend.php
@@ -7,11 +7,11 @@ function katex_init() {
 	global $katex_options;
 
 	if (is_array($katex_options) && isset($katex_options['katex_jsdelivr_setting']) && $katex_options['katex_jsdelivr_setting'] == 1) {
-		wp_register_style('katex', '//cdn.jsdelivr.net/katex/0.6.0/katex.min.css', false, null);
-		wp_register_script('katex', '//cdn.jsdelivr.net/katex/0.6.0/katex.min.js', array(), null, true);
+		wp_register_style('katex', '//cdn.jsdelivr.net/katex/0.7.0/katex.min.css', false, null);
+		wp_register_script('katex', '//cdn.jsdelivr.net/katex/0.7.0/katex.min.js', array(), null, true);
 	} else {
-		wp_register_style('katex', plugins_url('assets/katex.min.css', dirname(__FILE__)), false, '0.6.0');
-		wp_register_script('katex', plugins_url('assets/katex.min.js', dirname(__FILE__)), array(), '0.6.0', true);
+		wp_register_style('katex', plugins_url('assets/katex.min.css', dirname(__FILE__)), false, '0.7.0');
+		wp_register_script('katex', plugins_url('assets/katex.min.js', dirname(__FILE__)), array(), '0.7.0', true);
 	}
 }
 add_action('init', 'katex_init');
@@ -26,9 +26,9 @@ function katex_handler($atts, $content = null){
 	), $atts );
 
 	if ($latex_atts['display'] == 'true' || strpos($content, '\\displaystyle') === 0) {
-		return '<script type="text/katex" data-display="true">' . html_entity_decode($content) . '</script>';
+		return '<span class="wp-katex-eq katex-display" data-display="true">' . htmlspecialchars(html_entity_decode($content)) . '</span>';
 	} else {
-		return '<script type="text/katex" data-display="false">' . html_entity_decode($content) . '</script>';
+		return '<span class="wp-katex-eq" data-display="false">' . htmlspecialchars(html_entity_decode($content)) . '</span>';
 	}
 }
 add_shortcode('latex', 'katex_handler');
@@ -37,7 +37,7 @@ function katex_rubber() {
 	global $katex_using;
 	if ($katex_using) {
 	?>
-	<script>!function(){"use strict";var t=document.querySelectorAll("script[type='text/katex']");Array.prototype.forEach.call(t,function(t,e){var r={displayMode:"true"===t.getAttribute("data-display"),throwOnError:!1},a=document.createElement("span");try{katex.render(t.textContent,a,r)}catch(o){a.style.color="red",a.textContent=o.message}t.parentNode.replaceChild(a,t)})}();</script>
+	<script>!function(){"use strict";var e=document.querySelectorAll(".wp-katex-eq");Array.prototype.forEach.call(e,function(e){var t={displayMode:"true"===e.getAttribute("data-display"),throwOnError:!1},r=document.createElement("span");try{katex.render(e.textContent,r,t)}catch(a){r.style.color="red",r.textContent=a.message}e.parentNode.replaceChild(r,e)})}();</script>
 	<?php
 	}
 }

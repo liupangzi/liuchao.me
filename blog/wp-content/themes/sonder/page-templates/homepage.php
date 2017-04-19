7574375a602dca2b3252c13388d003456dd3f4ed
holyghost <thesedays@126.com>
2017-04-19 10:28:10 +0000

    change theme

--- blog/wp-content/themes/sonder/page-templates/homepage.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Template Name: Homepage
- */
-?>
-
-<?php while (have_posts()) : the_post(); ?>
-    <?php get_template_part('templates/content', 'homepage'); ?>
-<?php endwhile; ?>
--- blog/wp-content/themes/sonder/templates/content-page.php
+++
@@ -1,3 +0,0 @@
-<?php the_content(); ?>
-<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sonder'), 'after' => '</p></nav>']); ?>
-<?php comments_template('/templates/comments.php'); ?>
